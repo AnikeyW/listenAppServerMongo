00463438b45fe1f56a0e7d9aa38ce04d57fbebf5
AnikeeW <egor4ik048@gmail.com>
2024-02-07 00:20:10 +0300

    сервер с монго

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as path from "path";
+import {TrackModule} from "./track/track.module";
+import {FileModule} from "./file/file.module";
+import {MongooseModule} from "@nestjs/mongoose";
+import {ServeStaticModule} from "@nestjs/serve-static";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.evypqpo.mongodb.net/music-platform?retryWrites=true&w=majority'),
+    // MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.oeudk.mongodb.net/music-platform?retryWrites=true&w=majority'),
+    TrackModule,
+    FileModule
+  ],
 })
 export class AppModule {}
+// mongodb+srv://admin:<password>@cluster0.evypqpo.mongodb.net/?retryWrites=true&w=majority
