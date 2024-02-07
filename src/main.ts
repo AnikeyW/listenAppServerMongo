01d3431c550ed21a93c8429a97dba0e088340c94
AnikeeW <egor4ik048@gmail.com>
2024-02-07 14:47:12 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://listen-app-gamma.vercel.app', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
   await app.listen(5000);
 }
 bootstrap();
